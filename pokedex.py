5a0e5276fcc9d7785fd4b3b6a9934159acf25184
jsgreen03 <jsgreen@calpoly.edu>
2024-05-13 16:20:11 -0700

    Link CSV Generated

--- pokedex.py
+++ pokedex.py
@@ -4,6 +4,8 @@ import requests
 from lxml import html
 import xml.etree.ElementTree as ET 
 
+website = "https://www.serebii.net/"
+
 def main():
 	args = sys.argv
 	gen = args[1]
@@ -11,11 +13,44 @@ def main():
 		initialize9()
 
 def initialize9():
-	pass
-	response = requests.get("https://www.serebii.net/scarletviolet/paldeapokedex.shtml")
-	tree = html.fromstring(response.content)
+	mon_links = pd.read_csv("mon_links.csv",index_col="Names")
 
-	print(tree.xpath("/html/body/div[1]/div[2]/main/table[2]")[0])
+
+
+
+
+
+
+
+
+#def link_df_creator():
+#	paldea = requests.get("https://www.serebii.net/scarletviolet/paldeapokedex.shtml")
+#	paldea_tree = html.fromstring(paldea.content)
+#	mons = {}
+#	for i in range(2,402):
+#		imon = paldea_tree.xpath("/html/body/div[1]/div[2]/main/table[2]/tr[{}]/td[3]/a".format(i))[0]
+#		mons[imon.text]=website[:-1]+imon.attrib["href"]
+#	kitakami = requests.get("https://www.serebii.net/scarletviolet/kitakamipokedex.shtml")
+#	kitakami_tree = html.fromstring(kitakami.content)
+#	for j in range(2,202):
+#		jmon = kitakami_tree.xpath("/html/body/div[1]/div[2]/main/table[2]/tr[{}]/td[3]/a".format(j))[0]
+#		if jmon.text not in mons.keys():
+#		   mons[jmon.text]=website[:-1]+jmon.attrib["href"]
+#	blueberry = requests.get("https://www.serebii.net/scarletviolet/blueberrypokedex.shtml")
+#	blueberry_tree = html.fromstring(blueberry.content)
+#	for k in range(2,245):
+#		kmon = blueberry_tree.xpath("/html/body/div[1]/div[2]/main/table[2]/tr[{}]/td[3]/a".format(k))[0]
+#		if kmon.text not in mons.keys():
+#		   mons[kmon.text]=website[:-1]+kmon.attrib["href"]
+#	mon_links = pd.DataFrame.from_dict(mons, orient='index',columns=["Links"])
+#	mon_links.to_csv("C:/Users/jaxon/OneDrive/Desktop/PythonPokedex/mon_links.csv",)
+	
+
+
+
+	#for it in sprig.iter():
+	#print(sprig.attrib)
+	#print(sprig.text)
 
 
 
